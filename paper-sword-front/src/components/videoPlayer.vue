b064425624148adc35aaffb45e99df608a7329f0
luo <2825787448@qq.com>
2023-10-24 19:51:59 +0800

    new

---
+++ paper-sword-front/src/components/videoPlayer.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="container">
+        <video src="../assets/1-1.mp4" controls="controls" autoplay="autoplay" loop="loop"></video>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "videoPlayer"
+    }
+</script>
+
+<style scoped>
+.container{
+    width: 500px;
+    height: 800px;
+}
+video{
+    width: 100%;
+    height: 100%;
+}
+</style>

c80bbae478e35335051faece745ea2154dcdeb27
zzh <zzh@666.com>
2023-10-24 19:58:21 +0800

    框架搭建

---
+++ paper-sword-end/paper-sword-user/src/main/java/com/paper/sword/mapper/UserTMapper.java
@@ -0,0 +1,10 @@
+package com.paper.sword.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.paper.sword.entity.User;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserTMapper extends BaseMapper<User> {
+
+}

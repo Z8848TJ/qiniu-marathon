220acf31b82bcf93315e4f4fb06e6a0fa97346c0
王 文豪 <wangw.h-s@inspur.com>
2023-10-31 17:42:57 +0800

    2023/10/31 加入es模块与视频分析模块
    Signed-off-by: 王 文豪 <wangw.h-s@inspur.com>

---
+++ paper-sword-end/paper-sword-gate-way/src/main/java/com/paper/sword/config/Lableconfig.java
@@ -0,0 +1,24 @@
+package com.paper.sword.config;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * @author wwh
+ * @date 2023/10/30
+ */
+@Configuration
+@PropertySource("classpath:lable.properties")
+@ConfigurationProperties(prefix = "lable")
+@Data
+@Slf4j
+public class Lableconfig {
+
+    public String scriptPath;
+
+    public String outputDir;
+
+}
